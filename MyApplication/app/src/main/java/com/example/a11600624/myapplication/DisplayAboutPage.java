6433f78afb2c2979539308588d1ed4f73cd9cf1b
NielsVervloesem <Niels.Vervloesem@student.pxl.be>
2018-10-02 15:45:27 +0200

    Image, button and navigation example
    Early draft of startscreen with an image and couple of buttons and navigation to the about screen.

---
+++ MyApplication/app/src/main/java/com/example/a11600624/myapplication/DisplayAboutPage.java
@@ -0,0 +1,13 @@
+package com.example.a11600624.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayAboutPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_about_page);
+    }
+}
---
+++ MyApplication/app/src/main/java/com/example/a11600624/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.a11600624.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openAboutPage(View view){
+        Intent startNewActivity = new Intent(this, DisplayAboutPage.class);
+
+
+        startActivity(startNewActivity);
+    }
+
+}
