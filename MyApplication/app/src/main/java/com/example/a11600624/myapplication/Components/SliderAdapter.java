2054c87261feda90a6d8686aa5a5e6547d3e6f6a
Siebe Corstjens <siebe.corstjens@student.pxl.be>
2018-10-16 10:45:40 +0200

    Refactored several classes
    Added packages and refactored code for the pages

---
+++ MyApplication/app/src/main/java/com/example/a11600624/myapplication/Components/SliderAdapter.java
@@ -0,0 +1,74 @@
+package com.example.a11600624.myapplication.Components;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.example.a11600624.myapplication.Database.DatabaseHelper;
+import com.example.a11600624.myapplication.R;
+
+public class SliderAdapter extends PagerAdapter {
+    private Context context;
+    private DatabaseHelper databaseHelper;
+
+    private LayoutInflater layoutInflater;
+
+    private ImageView slideImageView;
+    private TextView slideHeading;
+    private TextView slideDescription;
+
+    public SliderAdapter(Context context) {
+        this.context = context;
+        databaseHelper = new DatabaseHelper(context);
+    }
+
+    public int[] slide_images = {
+            R.drawable.thanos_character,
+            R.drawable.thor_character,
+            R.drawable.hulk_character
+    };
+
+    @Override
+    public int getCount() {
+        return slide_images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+        return view == (RelativeLayout) o;
+    }
+
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.slide_layout, container, false);
+
+        slideImageView = (ImageView) view.findViewById(R.id.slideImage);
+        slideHeading = (TextView) view.findViewById(R.id.slideHeading);
+        slideDescription = (TextView) view.findViewById(R.id.slideDescription);
+
+        slideImageView.setImageResource(slide_images[position]);
+
+        Cursor data = databaseHelper.getCharacterById(position+1);
+
+        if (data.moveToFirst()) {
+            slideHeading.setText(data.getString(1).toUpperCase());
+            slideDescription.setText(data.getString(2));
+        }
+
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((RelativeLayout)object);
+    }
+}
--- MyApplication/app/src/main/java/com/example/a11600624/myapplication/Pages/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/a11600624/myapplication/Pages/MainActivity.java
@@ -1,19 +1,25 @@
-package com.example.a11600624.myapplication;
+package com.example.a11600624.myapplication.Pages;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.example.a11600624.myapplication.Database.DatabaseSeeder;
+import com.example.a11600624.myapplication.R;
+
 public class MainActivity extends AppCompatActivity {
+    DatabaseSeeder databaseSeeder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        databaseSeeder = new DatabaseSeeder(this);
     }
 
-    public void openAboutPage(View view){
+    public void openAboutPage(View view) {
         Intent startNewActivity = new Intent(this, DisplayAboutPage.class);
         startActivity(startNewActivity);
     }
