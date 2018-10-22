711f1cccf54cd9366a98cc16668e49801b32af79
Siebe Corstjens <siebe.corstjens@student.pxl.be>
2018-10-22 21:09:54 +0200

    Added security on NextButton

--- MyApplication/app/src/main/java/com/example/a11600624/myapplication/Pages/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/a11600624/myapplication/Pages/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.a11600624.myapplication.Database.DatabaseSeeder;
 import com.example.a11600624.myapplication.R;
@@ -11,12 +12,18 @@ import com.example.a11600624.myapplication.R;
 public class MainActivity extends AppCompatActivity {
     DatabaseSeeder databaseSeeder;
 
+    public static Button nextButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         databaseSeeder = new DatabaseSeeder(this);
+
+        nextButton = findViewById(R.id.button4);
+        nextButton.setEnabled(false);
+        nextButton.setText("Loading characters...");
     }
 
     public void openAboutPage(View view) {
